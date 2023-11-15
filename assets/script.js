e8a2eed528d170a83986767898a35184a74a871c
ismail hosen <ismailwdpf@gmail.com>
2023-11-16 00:32:19 +0600

    update

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('#menu-icon');
+let navabr = document.querySelector('.navbar');
 
-menu.onclick = ()=>{
+menu.onclick = () =>{
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    navabr.classList.toggle('open');
 };
 const sr = ScrollReveal({
     distance : '40px',
@@ -11,12 +11,12 @@ const sr = ScrollReveal({
     reset : true
 });
 sr.reveal('.logo',{delay:250, origin:'left'});
-sr.reveal('.navbar',{delay:300, origin:'top'});
-// sr.reveal('.menu-btn',{delay:200, origin:'top'});
+// sr.reveal('.navbar',{delay:400, origin:'top'});
+sr.reveal('.menu-btn',{delay:300, origin:'top'});
 sr.reveal('.home-text span',{delay:350, origin:'right'});
 sr.reveal('.home-text h1',{delay:300, origin:'right'});
 sr.reveal('.home-text p',{delay:350, origin:'left'});
-// sr.reveal('.main-btn',{delay:350, origin:'top'});
+sr.reveal('.main-btn',{delay:350, origin:'top'});
 sr.reveal('.home-img',{delay:300, origin:'left'});
 
 sr.reveal('.social',{delay:700, origin:'top'});
